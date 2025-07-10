b20092fbfa9dfe1f28eab74fd93e198aeaa2ba73
jayden <bjayden36@gmail.com>
2025-07-10 08:57:38 -0400

    create command now allows the user to specify a filepath for the CSV file with the -f flag

--- cmd/create.go
+++ cmd/create.go
@@ -6,17 +6,28 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
+
+var fpath string // CL will look for filepath if -f flag is passed
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Creates a new CSV file",
-	Long:  "Creates a CSV file which will contain all of the user's tasks.",
+	Long: `Creates a CSV file which will contain all of the user's tasks. 
+	Note, if -f is not included and a filepath is not given, the file will be made in the working directory.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		file, err := os.Create("tasks.csv")
+		if cmd.Flags().Changed("filepath") {
+			if fpath == "" {
+				fmt.Println("Please specify your filepath when using -f")
+				return
+			}
+		}
+		fpath = filepath.Join(fpath, "tasks.csv")
+		file, err := os.Create(fpath)
 		if err != nil {
 			fmt.Println("Error while trying to create file.")
 		}
@@ -27,6 +38,10 @@ var createCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(createCmd)
 
+	/*StringVarP will assign the arg directly after the flag to the variable specified in the function's parameters,
+	while StringP uses the value that is specified directly */
+
+	createCmd.Flags().StringVarP(&fpath, "filepath", "f", "", "Optional: specifies the path of the csv file (do not include the .csv file at the end).")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
