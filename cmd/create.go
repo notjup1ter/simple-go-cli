e2156747697aa17badf897a7e611845ec2a68de4
jayden <bjayden36@gmail.com>
2025-07-10 07:57:26 -0400

    implemented command to generate a new CSV file

---
+++ cmd/create.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Creates a new CSV file",
+	Long:  "Creates a CSV file which will contain all of the user's tasks.",
+	Run: func(cmd *cobra.Command, args []string) {
+		file, err := os.Create("tasks.csv")
+		if err != nil {
+			fmt.Println("Error while trying to create file.")
+		}
+		defer file.Close()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
